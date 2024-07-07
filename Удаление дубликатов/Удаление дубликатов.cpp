fc0b7198bc894d3a54b8e9a72d9914c0956b860f
younsiamed <younsiamed@gmail.com>
2024-07-07 19:50:25 +0300

    Lesson-5-5

---
+++ Удаление дубликатов/Удаление дубликатов.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    SetConsoleOutputCP(65001);
+    int n;
+    std::cout << "[IN]:\n";
+    std::cin >> n;
+
+    std::set<int> unique_numbers;
+    for (int i = 0; i < n; ++i) {
+        int num;
+        std::cin >> num;
+        unique_numbers.insert(num);
+    }
+
+    std::vector<int> sorted_numbers(unique_numbers.begin(), unique_numbers.end());
+
+    std::sort(sorted_numbers.begin(), sorted_numbers.end(), std::greater<int>());
+
+    std::cout << "[OUT]:\n";
+    for (int num : sorted_numbers) {
+        std::cout << num << std::endl;
+    }
+
+    return 0;
+}
---
+++ Частота символов/Частота символов.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+bool compare(const std::pair<char, int>& a, const std::pair<char, int>& b) {
+    if (a.second != b.second) {
+        return a.second > b.second;
+    }
+    return false;
+}
+
+int main() {
+    SetConsoleOutputCP(65001);
+    std::string input;
+    std::cout << "[IN]: ";
+    std::getline(std::cin, input);
+
+    std::map<char, int> frequency;
+    std::vector<char> order;
+
+    for (char c : input) {
+        if (frequency[c] == 0) {
+            order.push_back(c);
+        }
+        frequency[c]++;
+    }
+
+    std::vector<std::pair<char, int>> freqVec;
+    for (char c : order) {
+        freqVec.push_back(std::make_pair(c, frequency[c]));
+    }
+
+    std::sort(freqVec.begin(), freqVec.end(), compare);
+
+    std::cout << "[OUT]:\n";
+    for (const auto& pair : freqVec) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+
+    return 0;
+}
